b3b59021155d43e5ce99a8843f32c26e12535148
HBFLEX <happybanda@dyuni.ac.mw>
2024-06-18 17:30:59 +0200

    feature:add socket.io

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send
+import secrets
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = secrets.token_hex(32)
 
+socketIO = SocketIO(app)
 
 @app.route('/')
-def home():
-    return "<h1>Welcome to flask app</h1>"
+def home_page():
+    return render_template('index.html')
+
+
+@socketIO.on('message')
+def handleMessage(msg):
+    print(f'Message: {msg}')
+    send(msg, broadcast=True)
+
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketIO.run(app, debug=True)
+
